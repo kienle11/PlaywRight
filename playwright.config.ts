7ca5e1ead4ce934016949fe940724a961f38f1e8
Kien Le <letrungkien1611@gmail.com>
2026-05-08 14:29:49 +0700

    Updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,10 +23,11 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     screenshot: 'only-on-failure',
-    headless : false ,
+    headless : true ,
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
