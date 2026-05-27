a336fe7888fdb1dd02fa76f50c2117f78bae5e96
Kien Le <letrungkien1611@gmail.com>
2026-05-27 22:34:49 +0700

    Update CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,13 +14,13 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : 4,
+  workers: process.env.CI ? 2 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
 
@@ -43,15 +43,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome']},
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox']},
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox']},
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
